5bb08ada21c36ac4e97695e8657ed2c907a8f599
Mikhail Molovtsev <mishamolovtsev@gmail.com>
2020-06-11 11:00:19 +0300

    Some fixes

--- train.py
+++ train.py
@@ -2,17 +2,20 @@ import logging
 import os
 import datetime
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+
 import cv2
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import models, losses, optimizers, preprocessing, metrics
+from tensorflow.keras import models, losses, optimizers, preprocessing, metrics, initializers, activations
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.layers import Input, Conv2D, MaxPool2D, Flatten, Dense, GlobalAveragePooling2D, Dropout, ZeroPadding2D, BatchNormalization, Activation
 from tensorflow.keras.utils import plot_model
+import click
 
-from util_functions import conv_block, ident_block
+from util_functions import conv_block, ident_block, data_augment
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+
 
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -21,48 +24,59 @@ logging.basicConfig(
 
 log = logging.getLogger(__name__)
 
+@click.command()
+@click.option('--batch-size', type=int, help='Batch size for train set', default=16)
+@click.option('--epochs-count', type=int, help='Epochs count for model fit', default=10)
+@click.option('--learning-rate', type=float, help='Learning rate for optimizer', default=0.001)
+@click.option('--alpha', type=float, help='Parameter for leackyReLU function', default=0.0)
+@click.option('--classes-rate', type=float, help='Rate of classes from place_365', default=0.05)
+@click.option('--data-dir', type=str, help='Directory with data')
+def main(batch_size, epochs_count, learning_rate, alpha, classes_rate, data_dir):
 
-def main():
-
-    batch_size = 32
-    epoch_count = 100
-    learning_rate = 0.01
     fc_dropout_rate = 0.5
+    conv_dropout_rate = 0.7
+    l2_reg = 0.01
+
+    target_size = [128, 128, 1]
+
+    elu = lambda x: activations.elu(x, alpha=alpha)
+    #augment = lambda image: data_augment(image, target_size)
 
     log_dir = 'logs/fit/' + datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
     tf_board_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-    classes_list = [
-        '_v_veterinarians_office', '_m_mountain', '_c_carrousel',
-        '_s_stage_outdoor', '_l_lake_natural', '_p_pond',
-        '_s_swimming_pool_indoor', '_m_museum_indoor', '_e_embassy',
-        '_w_windmill', '_a_assembly_line', '_b_bridge',
-        '_r_recreation_room', '_f_flea_market_indoor', '_p_pharmacy',
-        '_t_topiary_garden', '_p_pizzeria'
-    ]
+
+    train_dir = '/'.join([data_dir, 'train_set'])
+    val_dir = '/'.join([data_dir, 'val_set'])
+    test_dir = '/'.join([data_dir, 'test_set'])
+    all_classes = np.array(os.listdir(train_dir))
+    classes_list = all_classes[np.random.uniform(size=len(all_classes)) <= classes_rate].tolist()
+    log.info(f'Model will for classes: {classes_list}')
 
     log.info('Data generator creation')
 
-    data_generator = ImageDataGenerator()
+    data_generator = ImageDataGenerator(
+        rescale=1./255
+    )
     train_generator = data_generator.flow_from_directory(
-        directory='/home/mightyracoon/datasets/place_365/train_set',
+        directory=train_dir,
         target_size=(256, 256),
         classes=classes_list,
         batch_size=batch_size
     )
     val_generator = data_generator.flow_from_directory(
-        directory='/home/mightyracoon/datasets/place_365/val_set',
+        directory=val_dir,
         target_size=(256, 256),
         classes=classes_list
     )
 
     log.info('Model Creation')
 
-    input = Input(shape=(256, 256, 3))
+    model_input = Input(shape=(256, 256, 3))
 
-    x = ZeroPadding2D(padding=(3, 3))(input)
-    x = Conv2D(filters=64, kernel_size=(7, 7), strides=(2, 2), padding='same')(x)
+    x = ZeroPadding2D(padding=(3, 3))(model_input)
+    x = Conv2D(filters=64, kernel_size=(7, 7), strides=(2, 2), padding='same', kernel_initializer='he_uniform')(x)
     x = BatchNormalization()(x)
-    x = Activation(activation='relu')(x)
+    x = Activation(activation=elu)(x)
     x = ZeroPadding2D(padding=(1, 1))(x)
     x = MaxPool2D(pool_size=(3, 3), strides=(2, 2))(x)
 
@@ -70,45 +84,37 @@ def main():
     filters_1 = [64, 64, 256]
     kernels_1 = [(1, 1), (3, 3), (1, 1)]
 
-    x = conv_block(x, filters_1, kernels_1, (1, 1))
-    x = ident_block(x, filters_1, kernels_1)
-    x = ident_block(x, filters_1, kernels_1)
+    x = conv_block(x, filters_1, kernels_1, (1, 1), alpha=alpha)
+    x = ident_block(x, filters_1, kernels_1, (1, 1), alpha=alpha)
+    x = ident_block(x, filters_1, kernels_1, (1, 1), alpha=alpha)
+    x = Dropout(rate=conv_dropout_rate)(x)
 
     # Conv block 2
-    filters_2 = [126, 128, 512]
+    filters_2 = [128, 128, 512]
     kernels_2 = [(1, 1), (3, 3), (1, 1)]
 
-    x = conv_block(x, filters_2, kernels_2)
-    x = ident_block(x, filters_2, kernels_2)
-    x = ident_block(x, filters_2, kernels_2)
+    x = conv_block(x, filters_2, kernels_2, (1, 1), alpha=alpha)
+    x = ident_block(x, filters_2, kernels_2, (1, 1), alpha=alpha)
+    x = ident_block(x, filters_2, kernels_2, (1, 1), alpha=alpha)
+    x = Dropout(rate=conv_dropout_rate)(x)
 
     # Conv block 3
     filters_3 = [256, 256, 1024]
     kernels_3 = [(1, 1), (3, 3), (1, 1)]
 
-    x = conv_block(x, filters_3, kernels_3)
-    x = ident_block(x, filters_3, kernels_3)
-    x = ident_block(x, filters_3, kernels_3)
-    x = ident_block(x, filters_3, kernels_3)
-    x = ident_block(x, filters_3, kernels_3)
-    x = ident_block(x, filters_3, kernels_3)
-
-    #  Conv block 4
-    filters_4 = [512, 512, 2048]
-    kernels_4 = [(1, 1), (3, 3), (1, 1)]
-
-    x = conv_block(x, filters_4, kernels_4)
-    x = ident_block(x, filters_4, kernels_4)
-    x = ident_block(x, filters_4, kernels_4)
+    x = conv_block(x, filters_3, kernels_3, (1, 1), alpha=alpha)
+    x = ident_block(x, filters_3, kernels_3, (1, 1), alpha=alpha)
+    x = ident_block(x, filters_3, kernels_3, (1, 1), alpha=alpha)
+    x = Dropout(rate=conv_dropout_rate)(x)
 
     x = GlobalAveragePooling2D()(x)
     x = Flatten()(x)
 
-    x = Dense(units=1000, activation='relu')(x)
+    x = Dense(units=1000, activation=elu, kernel_initializer='he_uniform')(x)
     x = Dropout(rate=fc_dropout_rate)(x)
-    output = Dense(units=len(classes_list), activation='softmax')(x)
+    model_output = Dense(units=len(classes_list), activation='softmax')(x)
 
-    model = models.Model(input, output)
+    model = models.Model(model_input, model_output)
 
     model.summary()
     plot_model(model, show_shapes=True)
@@ -117,26 +123,29 @@ def main():
     model.compile(
         optimizer=optimizers.Adam(learning_rate=learning_rate),
         loss=losses.CategoricalCrossentropy(from_logits=True),
-        metrics=[metrics.Accuracy(), metrics.AUC()]
+        metrics=[metrics.Accuracy(), metrics.Precision(), metrics.Recall(), metrics.AUC()]
     )
 
     log.info('Model fitting')
     model.fit(
         x=train_generator,
         validation_data=val_generator,
-        epochs=epoch_count,
+        epochs=epochs_count,
         callbacks=[tf_board_callback]
     )
 
     log.info('Model testing')
-    test_generator = data_generator.flow_from_directory(
-        directory='/home/mightyracoon/datasets/place_365/test_set',
+    test_data_generator = ImageDataGenerator(
+        rescale=1. / 255
+    )
+    test_generator = test_data_generator.flow_from_directory(
+        directory=test_dir,
         target_size=(256, 256),
         classes=classes_list
     )
 
     model_test_results = model.evaluate(x=test_generator)
-    log.info(f'Model loss: {model_test_results[0]}, accuracy: {model_test_results[1]}. AUC: {model_test_results[2]}')
+    log.info(f'Model loss: {model_test_results[0]}, accuracy: {model_test_results[1]}. AUC: {model_test_results[4]}, Presicion: {model_test_results[4]}, Recall: {model_test_results[3]}')
 
 
 if __name__ == '__main__':
--- util_functions.py
+++ util_functions.py
@@ -1,41 +1,58 @@
+import Augmentor as augmentor
+import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, add
+from tensorflow.keras import activations
 
 
-def conv_block(input_, filters, kernels, strides=(2,2)):
+def conv_block(input_, filters, kernels, strides=(2, 2), alpha=0.2):
 
-    x = Conv2D(filters=filters[0], kernel_size=kernels[0], strides=(1, 1), padding='same')(input_)
-    x = BatchNormalization()(x)
-    x = Activation(activation='relu')(x)
-    x = Conv2D(filters=filters[1], kernel_size=kernels[1], strides=strides, padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Activation(activation='relu')(x)
-    x = Conv2D(filters=filters[2], kernel_size=kernels[2], strides=(1, 1), padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Activation(activation='relu')(x)
+    elu = lambda x: activations.elu(x, alpha=alpha)
 
-    shortcut = Conv2D(filters=filters[2], kernel_size=kernels[2], strides=strides, padding='same')(input_)
+    x = input_
+
+    for filters_count, kernel in zip(filters, kernels):
+        x = Conv2D(filters=filters_count, kernel_size=kernel, strides=strides, padding='same',
+                   kernel_initializer='he_uniform')(x)
+        x = BatchNormalization()(x)
+        x = Activation(activation=elu)(x)
+
+    shortcut = Conv2D(filters=filters.copy().pop(), kernel_size=kernels.copy().pop(), strides=strides, padding='same',
+                      kernel_initializer='he_uniform')(input_)
     shortcut = BatchNormalization()(shortcut)
-    shortcut = Activation(activation='relu')(shortcut)
+    shortcut = Activation(activation=elu)(shortcut)
 
     conv_block_res = add([x, shortcut])
-    conv_block_res_active = Activation(activation='relu')(conv_block_res)
+    conv_block_res_active = Activation(activation=elu)(conv_block_res)
 
     return conv_block_res_active
 
 
-def ident_block(input_, filters, kernels, strides=(1, 1)):
+def ident_block(input_, filters, kernels, strides=(1, 1), alpha=0.2):
 
-    x = Conv2D(filters=filters[0], kernel_size=kernels[0], strides=strides, padding='same')(input_)
-    x = BatchNormalization()(x)
-    x = Activation(activation='relu')(x)
-    x = Conv2D(filters=filters[1], kernel_size=kernels[1], strides=strides, padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Activation(activation='relu')(x)
-    x = Conv2D(filters=filters[2], kernel_size=kernels[2], strides=strides, padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Activation(activation='relu')(x)
+    elu = lambda x: activations.elu(x, alpha=alpha)
 
-    conv_block_res = add([x, input_])
-    conv_block_res_active = Activation(activation='relu')(conv_block_res)
+    x = input_
 
-    return conv_block_res_active
+    for filters_count, kernel in zip(filters, kernels):
+        x = Conv2D(filters=filters_count, kernel_size=kernel, strides=strides, padding='same',
+                   kernel_initializer='he_uniform')(x)
+        x = BatchNormalization()(x)
+        x = Activation(activation=elu)(x)
+
+    ident_block_res = add([x, input_])
+    ident_block_res = Activation(activation=elu)(ident_block_res)
+
+    return ident_block_res
+
+
+def convert(image):
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    return image
+
+
+def data_augment(image, target_dim):
+    image = convert(image)
+    if target_dim[2] == 1:
+        image = tf.image.rgb_to_grayscale(image)
+    image = tf.image.resize(images=image, size=(target_dim[0], target_dim[1]))
+    return image
